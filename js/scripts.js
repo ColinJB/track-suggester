a75cefc7d6b713aa6f2eaf1fc33fec8ea583436c
Colin Bloom <colinjbloom113@gmail.com>
2017-02-17 11:40:00 -0800

    add form containing two selects, 3 radio buttons question, submit ubtton, beginning of variable result algorithm

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-$(function() {
+$(document).ready(function() {
+  $("form#survey").submit(function(event) {
+
+    var result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name]"))
+
+    if {
+
+    }
 
 
+  });
 });
