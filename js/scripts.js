fd93ea667a8ff7513c3323a8ccd1e8f29ec349a0
Colin Bloom <colinjbloom113@gmail.com>
2017-02-17 12:05:54 -0800

    add finished result variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
+  var result;
+
   $("form#survey").submit(function(event) {
 
-    var result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name]"))
 
-    if {
 
-    }
+    result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name=demand]:checked").val()) + parseInt($("input:radio[name=content]:checked").val()) + parseInt($("input:radio[name=quick]:checked").val());
 
+    console.log("result is: " + result);
+
+    event.preventDefault();
 
   });
 });
