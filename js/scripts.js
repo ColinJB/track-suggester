3445d9c3b043a6d81fa59b8ff41d6f28a5e08408
Colin Bloom <colinjbloom113@gmail.com>
2017-02-17 14:16:24 -0800

    add branch for results, add JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-  var result;
+
+  var result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name=demand]:checked").val()) + parseInt($("input:radio[name=content]:checked").val()) + parseInt($("input:radio[name=quick]:checked").val());;
+  var resultTrack;
 
   $("form#survey").submit(function(event) {
 
+    var name = $("input#studentName").val();
+    var email = $("input#studentEmail").val();
 
+    if (result >= 50) {
+      resultTrack = "C#";
+    } else if (result < 50 && result >= 25) {
+        resultTrack = "Java";
+    } else if (result < 25 && result > 2) {
+        resultTrack = "PHP";
+    } else {
+        resultTrack = "Ruby";
+    }
 
-    result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name=demand]:checked").val()) + parseInt($("input:radio[name=content]:checked").val()) + parseInt($("input:radio[name=quick]:checked").val());
+    $("#resultOutput").show();
+    $("#resultTrack").text(resultTrack);
+    $("#nameOutput").text(name);
+    $("#emailOutput").text(email);
 
-    console.log("result is: " + result);
-
+    console.log("name is: " + name);
+    console.log("email is: " + email);
     event.preventDefault();
 
   });
