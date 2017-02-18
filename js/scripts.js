099039369763cf20cf1e4d9ca75f7386c640b757
Colin Bloom <colinjbloom113@gmail.com>
2017-02-17 16:30:11 -0800

    add css styling: borders, jumbotron background

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
         resultTrack = "Ruby";
     }
 
+
     $("#resultOutput").fadeIn();
     $("#resultTrack").text(resultTrack);
     $("#nameOutput").text(name);
