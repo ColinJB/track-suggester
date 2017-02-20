b1d92268247bb7cf11b07ff5acfd31e014624979
Colin Bloom <colinjbloom113@gmail.com>
2017-02-20 14:10:04 -0800

    fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-
-  var result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name=demand]:checked").val()) + parseInt($("input:radio[name=content]:checked").val()) + parseInt($("input:radio[name=quick]:checked").val());;
-  var resultTrack;
-
   $("form#survey").submit(function(event) {
+
+    var result = parseInt($("select#company").val()) + parseInt($("select#software").val()) + parseInt($("input:radio[name=demand]:checked").val()) + parseInt($("input:radio[name=content]:checked").val()) + parseInt($("input:radio[name=quick]:checked").val());;
+
+    var resultTrack;
 
     var name = $("input#studentName").val();
     var email = $("input#studentEmail").val();
